f6f6ba86d3072283e03538d2e1462b727389cb99
mmevy <mervemetinogluu@gmail.com>
2021-09-22 21:35:03 +0300

    eslint bugs fixed

--- src/js/card.js
+++ src/js/card.js
@@ -3,78 +3,18 @@ import Duck from './Duck';
 
 // creating instances from Dog and Duck classes
 const animalsList = [
-  new Dog(
-    'Oliver',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    6,
-  ),
-  new Dog(
-    'Blue',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    4,
-  ),
-  new Dog(
-    'Ace',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    2,
-  ),
-  new Dog(
-    'Buddy',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    1,
-  ),
-  new Dog(
-    'Archie',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    2,
-  ),
-  new Dog(
-    'Leo',
-    'https://source.unsplash.com/collection/2109136/500x700?sig=',
-    4,
-    3,
-  ),
-  new Duck(
-    'Waddles',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    1,
-  ),
-  new Duck(
-    'Grey',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    1.5,
-  ),
-  new Duck(
-    'Rocky',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    2,
-  ),
-  new Duck(
-    'Daisy',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    2,
-  ),
-  new Duck(
-    'Puddle',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    1,
-  ),
-  new Duck(
-    'Daffy',
-    'https://source.unsplash.com/collection/4502239/500x700?sig=',
-    2,
-    3,
-  ),
+  new Dog('Oliver', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 6),
+  new Dog('Blue', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 4),
+  new Dog('Ace', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 2),
+  new Dog('Buddy', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 1),
+  new Dog('Archie', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 2),
+  new Dog('Leo', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 3),
+  new Duck('Waddles', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1),
+  new Duck('Grey', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1.5),
+  new Duck('Rocky', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 2),
+  new Duck('Daisy', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 2),
+  new Duck('Puddle', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1),
+  new Duck('Daffy', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 3),
 ];
 
 // creating and displaying grid layout with animals instances into the html
