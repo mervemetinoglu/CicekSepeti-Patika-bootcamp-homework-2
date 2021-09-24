006727232e1c271a847dc57ab63509819f8235a8
mmevy <mervemetinogluu@gmail.com>
2021-09-24 11:45:30 +0300

    created img src variables

--- src/js/card.js
+++ src/js/card.js
@@ -1,20 +1,23 @@
 import Dog from './Dog';
 import Duck from './Duck';
 
+const dogImgSrc = 'https://source.unsplash.com/collection/2109136/500x700?sig=';
+const duckImgSrc = 'https://source.unsplash.com/collection/4502239/500x700?sig=';
+
 // creating instances from Dog and Duck classes
 const animalsList = [
-  new Dog('Oliver', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 6),
-  new Dog('Blue', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 4),
-  new Dog('Ace', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 2),
-  new Dog('Buddy', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 1),
-  new Dog('Archie', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 2),
-  new Dog('Leo', 'https://source.unsplash.com/collection/2109136/500x700?sig=', 4, 3),
-  new Duck('Waddles', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1),
-  new Duck('Grey', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1.5),
-  new Duck('Rocky', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 2),
-  new Duck('Daisy', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 2),
-  new Duck('Puddle', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 1),
-  new Duck('Daffy', 'https://source.unsplash.com/collection/4502239/500x700?sig=', 2, 3),
+  new Dog('Oliver', dogImgSrc, 6),
+  new Dog('Blue', dogImgSrc, 4),
+  new Dog('Ace', dogImgSrc, 2),
+  new Dog('Buddy', dogImgSrc, 1),
+  new Dog('Archie', dogImgSrc, 2),
+  new Dog('Leo', dogImgSrc, 3),
+  new Duck('Waddles', duckImgSrc, 1),
+  new Duck('Grey', duckImgSrc, 1.5),
+  new Duck('Rocky', duckImgSrc, 2),
+  new Duck('Daisy', duckImgSrc, 2),
+  new Duck('Puddle', duckImgSrc, 1),
+  new Duck('Daffy', duckImgSrc, 3),
 ];
 
 // creating and displaying grid layout with animals instances into the html
@@ -23,25 +26,21 @@ function displayCards() {
   const gridDOM = document.querySelector('.grid');
 
   const htmlStringCard = animalsList
-    .map((item, index) => {
+  .map((item, index) => {
       let random = Math.random();
       random += index;
 
-      return `
-          <div class="grid__item">
-              <div class="grid__item-img">
-                  <img
-                  src="${item.image + random}
-                  "/>
-              </div>
-              <div class="grid__item-body">
-                  <h2>${item.name}</h2>
-                  <p>Age: ${item.age}</p>
-              </div>
-        </div>
-  `;
-    })
-    .join('');
+      return `<div class="grid__item">
+                <div class="grid__item-img">
+                    <img src="${item.image + random}"/>
+                </div>
+                <div class="grid__item-body">
+                    <h2>${item.name}</h2>
+                    <p>Age: ${item.age}</p>
+                    <p>Number of legs: ${item.numberOfLegs}</p>
+                </div>
+              </div>`;
+    }).join('');
 
   gridDOM.innerHTML = htmlStringCard;
   containerDOM.appendChild(gridDOM);
